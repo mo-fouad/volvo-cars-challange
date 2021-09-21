3b127f322d9a37537b8c09736ce87b62f686185f
mo-fouad <mo.fouad.saga@gmail.com>
2021-09-22 00:01:42 +0200

    WIP - Adding Support for IE11, Also add Volvo FavIcons

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
+/*
+Adding support For IE11 since it still in use for around 0.5%
+https://gs.statcounter.com/browser-market-share
+*/
+
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+
 import React from "react";
 import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
 import { styleRenderer, StyleProvider, ThemePicker } from "vcc-ui";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { getFavIcons } from "@volvo-cars/favicons";
 
 const renderer = styleRenderer();
 
@@ -13,9 +23,22 @@ renderer.renderStatic(
   "a"
 );
 
+const favIcons = getFavIcons();
+
 ReactDOM.render(
   <StyleProvider renderer={renderer}>
     <ThemePicker variant="light">
+      <Helmet>
+        <title>Volvo Cars Application</title>
+        <meta
+          name="description"
+          content="Volvo Cars Application to show the latest rechargeable cars"
+        />
+        <meta name="theme-color" content="#rgb(28 107 186)" />
+        {favIcons.map((icon, id) => (
+          <link key={`icon-${id}`} {...icon} />
+        ))}
+      </Helmet>
       <App />
     </ThemePicker>
   </StyleProvider>,
